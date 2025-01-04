0a4dfd5df2035ab164b6535ea05eb5e6d62c630d
Afeez-Byte <78375193+Afeez-Byte@users.noreply.github.com>
2025-01-03 19:28:01 -0500

    JAVA ROCK PAPER SCISSORS FILE

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+   
+   Random random = new Random();
+   Scanner scanner = new Scanner(System.in);
+   
+   int  x = 1;  
+   
+   while (x >= 10) {
+
+   System.out.println("0 = Rock");
+   System.out.println("1 = Paper");
+   System.out.println("2 = Scissors");
+
+   int  randomChoice = random.nextInt(3);
+   
+   System.out.println("Enter your choice: ");
+   int number = scanner.nextInt();
+
+   String[] choices = {"Rock", "Paper", "Scissors"};
+
+   System.out.println("You chose: " + choices[number]);
+   System.out.println("I choose: " + choices[randomChoice]);
+
+   if(number == 0 && randomChoice == 1) {
+
+    System.out.println("You Lose");
+} else if (number == 1 && randomChoice == 2) {
+
+   System.out.println("You Lose");
+} else if (number == 2 && randomChoice == 0) {
+
+   System.out.println("You Lose");
+} else if (number == 1 && randomChoice == 0) {
+
+   System.out.println("You Win");
+} else if (number == 0 && randomChoice == 2) {
+
+   System.out.println("You Win");
+} else if (number == 2 && randomChoice == 1) {
+
+   System.out.println("You Win");
+} else {
+
+   System.out.println("Its a Tie");
+}
+  } 
+}
+}
+
